1efd2f4ec0a804db10ec9ffe5d7a32287f2f63f5
c2700 <rohanphilip.india@gmail.com>
2022-11-07 02:04:04 +0530

    changes

---
+++ tkinter/db_ops.py
@@ -0,0 +1,26 @@
+from mysql.connector import *
+
+def FetchProduct():
+    pass
+
+def FetchProductCount():
+    pass
+
+def FetchAllProducts():
+    pass
+
+def FetchComponentsPerProduct():
+    pass
+
+def FetchComponent():
+    pass
+
+def FetchComponentCount():
+    pass
+
+def FetchAllComponents():
+    pass
+
+# def Fetch():
+#     pass
+
---
+++ tkinter/manip.py
@@ -0,0 +1,1 @@
+## file to manipulate qunatity & state of all products & components
